fd481e8130168c4f7bfc16e617c5b76172f54e90
Baaartosz <bartosz_p@hotmail.com>
2022-06-28 14:38:18 +0100

    update

--- src/main/java/com/sparta/lex/Main.java
+++ src/main/java/com/sparta/lex/Main.java
@@ -4,4 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static void mostCommon(String input) {
+        List<String> words = new ArrayList<>(Arrays.asList(input.split(" ")));
+        Map<Character, Long> charFrequency = words
+                .stream()
+                .flatMap(a -> a.chars().mapToObj(c -> (char) c))
+                .collect(
+                        Collectors.groupingBy(Function.identity(), Collectors.counting())
+                );
+        System.out.println(charFrequency);
+    }
 }
