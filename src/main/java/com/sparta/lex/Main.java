ecaf4c336bd1a7bbe7bd9a9ab16288187cb6e6e6
Baaartosz <bartosz_p@hotmail.com>
2022-06-28 14:38:51 +0100

    main

--- src/main/java/com/sparta/lex/Main.java
+++ src/main/java/com/sparta/lex/Main.java
@@ -1,8 +1,21 @@
 package com.sparta.lex;
 
+/*
+    https://github.com/Baaartosz/LexAnalyser
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-
+        LexAnalyser lexAnalyser = new LexAnalyser(ReadFile.getData("C:\\Dev\\Sparta Global\\Analyser\\src\\main\\java\\com\\sparta\\lex\\shortDoc.txt"));
+        System.out.println(lexAnalyser.mostCommonChar("The aliens are coming"));
+        mostCommon("HELLO HOW GOES IT");
     }
 
     public static void mostCommon(String input) {
