7ebf8c52784fe271475184f964efec1fd0703d29
Baaartosz <bartosz_p@hotmail.com>
2022-06-28 11:24:12 +0100

    init

---
+++ src/main/java/com/sparta/lex/LexAnalyser.java
@@ -0,0 +1,17 @@
+package com.sparta.lex;
+
+public class LexAnalyser {
+
+    /*
+    TODO [ ] number of chars
+    TODO [ ] number of words
+    TODO [ ] number of occurrences of a letter
+    TODO [ ] most frequently found letters
+
+    TODO [ ]
+
+    MUST case insensitive and non-alpha ignored
+
+     */
+
+}
